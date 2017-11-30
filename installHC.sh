90db6fbf6879ed56bcc7dce3929adc7c0fd386e5
Harrison_Chen <harrison_chen@trend.com.tw>
2017-11-30 01:38:57 -0500

    Add dsm folder to installhc.sh

--- installHC.sh
+++ installHC.sh
@@ -2,7 +2,7 @@
 /opt/dsm/dsm_c -action changesetting -name settings.configuration.webserviceAPIEnabled -Value true
 /opt/dsm/dsm_c -action changesetting -name settings.configuration.agentInitiatedActivation -Value 1
 /opt/dsm/dsm_c -action changesetting -name settings.configuration.agentInitiatedActivationActiveHost -Value 2
-/opt/dsm_s start
+/opt/dsm/dsm_s start
 #Enable HC server
 cd /opt/
 wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u152-b16/aa0333dd3019491ca4f6ddbe78cdb6d0/jdk-8u152-linux-x64.rpm"
