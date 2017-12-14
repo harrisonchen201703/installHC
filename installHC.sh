4cd43d8ce3d7ac6920941f2ccd0d2783be657205
Harrison_Chen <harrison_chen@trend.com.tw>
2017-12-13 23:20:53 -0500

    Add AMP with dev mode

--- installHC.sh
+++ installHC.sh
@@ -3,7 +3,9 @@
 /opt/dsm/dsm_c -action changesetting -name settings.configuration.agentInitiatedActivation -Value 1
 /opt/dsm/dsm_c -action changesetting -name settings.configuration.agentInitiatedActivationActiveHost -Value 2
 /opt/dsm/dsm_c -action changesetting -name settings.configuration.defaultHeartbeatPeriod -value 1
-/opt/dsm/dsm_s start
+echo "mode.developer=true" >> /opt/dsm/webclient/webapps/ROOT/WEB-INF/dsm.properties
+echo "mode.developer.key=e32e9a2e-4f24-409e-a751-39cf2db03b19" >> /opt/dsm/webclient/webapps/ROOT/WEB-INF/dsm.properties
+/opt/dsm/dsm_s restart
 #Enable HC server
 cd /opt/
 wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u152-b16/aa0333dd3019491ca4f6ddbe78cdb6d0/jdk-8u152-linux-x64.rpm"
